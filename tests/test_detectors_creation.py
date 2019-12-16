823df742801e80ab3f66ff1ecc8b8cd2a9b3a453
Dave Bakshani <dbakshani@expedia.com>
2019-12-16 12:25:56 -0800

    Add quartile threshold calculator code and test
    Rename standard deviation calculator method.

--- tests/test_detectors_creation.py
+++ tests/test_detectors_creation.py
@@ -6,38 +6,49 @@ import numpy as np
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
-def test_get_sample_std_dev_for_integers():
+def test_get_calculate_sigma_for_integers():
     sample = [5, 4, 7, 9, 15, 1, 0]
-    assert isclose(sample_std_dev(sample), 5.1130086, rel_tol=0.0001)
+    assert isclose(calculate_sigma(sample), 5.1130086, rel_tol=0.0001)
 
-def test_get_sample_std_dev_for_floats():
+def test_get_calculate_sigma_for_floats():
     sample = [35.2, 42.9, 37.8, 9.3, 15.0, 10.1, 20]
-    assert isclose(sample_std_dev(sample), 14.015671, rel_tol=0.0001)
+    assert isclose(calculate_sigma(sample), 14.015671, rel_tol=0.0001)
 
-def test_get_sample_std_dev_raises_error_for_single_value():
+def test_get_calculate_sigma_raises_error_for_single_value():
     sample = [35.2]
     with pytest.raises(Exception) as exception:
-        assert isclose(sample_std_dev(sample), 0)
+        assert isclose(calculate_sigma(sample), 0)
     assert str(exception.value) == "Sample must have at least two elements"
 
-def test_get_sample_std_dev_raises_error_with_empty_list():
+def test_get_calculate_sigma_raises_error_with_empty_list():
     with pytest.raises(Exception) as exception:
-        assert isclose(sample_std_dev([]), 0)
+        assert isclose(calculate_sigma([]), 0)
     assert str(exception.value) == "Sample must have at least two elements"
 
-def test_calculate_quartiles_without_multipliers():
+def test_calculate_quartiles():
     sample = [2, 5, 6, 7, 10, 22, 13, 14, 16, 65, 45, 12]
     q1, median, q3 = calculate_quartiles(sample)
     assert q1 == 6.5
     assert median == 12.5
     assert q3 == 19.0
 
+def test_calculate_quartile_thresholds():
+    q1, q3 = 2, 5
+    multiplier = 1.5
+    upper, lower = calculate_quartile_thresholds(q1, q3, multiplier)
+    assert upper == 9.5 # (q3 + (q3 - q1) * 1.5)
+    assert lower == -2.5 # (q1 - (q3 - q1) * 1.5)
+
+# TODO: weak and strong outliers, using provided multiplier
+# using sigmas to find outliers
+# using percentiles to find outliers
+
 
 ################################################################################
 ## implementation
 ################################################################################
 
-def sample_std_dev(sample):
+def calculate_sigma(sample):
     if len(sample) < 2:
         raise Exception("Sample must have at least two elements")
     array = np.array(sample)
@@ -46,3 +57,9 @@ def sample_std_dev(sample):
 def calculate_quartiles(sample):
     array = np.array(sample)
     return np.percentile(array, [25, 50, 75], interpolation='midpoint')
+
+def calculate_quartile_thresholds(q1, q3, multiplier):
+    iqr = q3 - q1
+    upper = q3 + iqr * multiplier
+    lower = q1 - iqr * multiplier
+    return upper, lower
