80bbfc77f71a0230fadf27f94d700dff68cbe42e
Prapoorna <pkotha@expediagroup.com>
2020-03-05 10:51:52 -0800

    MODIFY the exisitng CT model to work with highwatermark strategy

--- adaptive_alerting_detector_build/detectors/__init__.py
+++ adaptive_alerting_detector_build/detectors/__init__.py
@@ -2,4 +2,3 @@ from .base import Detector, DetectorUUID
 from .factory import build_detector, from_json
 from .client import DetectorClient
 from .constant_threshold import ConstantThresholdDetector
-from .ct_highwatermark import CTHighwatermarkDetector
--- tests/test_constant_detector_highwatermark.py
+++ tests/test_constant_detector_highwatermark.py
@@ -16,29 +16,23 @@ class TestCTHighwatermarkDetector:
     def test_train_detector_for_cgp_metrics(self):
         detector_config = dict(
             hyperparams=dict(upper_weak_multiplier=1.05, upper_strong_multiplier=1.10, 
-                hampel_window_size=10, hampel_n_sigma=3),
-            detectorMetaData=dict(subtype="ct-highwatermark-detector")
+                hampel_window_size=10, hampel_n_sigma=3, strategy="highwatermark")
         )
         detector_class = build_detector("constant-detector", detector_config)
         data = read_csv("./tests/data/latency_cgp_tp99.csv", header=0, usecols=[1], squeeze=True)
-        detector_class.train(data, "LATENCY_AVG")
+        detector_class.train(data, "LATENCY")
         print("detector model data after training:\n", related.to_json(detector_class))
-        assert isclose(detector_class.config.params.thresholds.upper_strong_threshold, 43711.030462, rel_tol=0.0001)
-        assert isclose(detector_class.config.params.thresholds.upper_weak_threshold, 41724.165441, rel_tol=0.0001)
+        assert isclose(detector_class.config.params.thresholds.strong_upper_threshold, 43711.030462, rel_tol=0.0001)
+        assert isclose(detector_class.config.params.thresholds.weak_upper_threshold, 41724.165441, rel_tol=0.0001)
 
     def test_train_detector_for_elb_metrics(self):
         detector_config = dict(
-                hyperparams=dict(upper_weak_multiplier=1.05, upper_strong_multiplier=1.10, 
-                    hampel_window_size=10, hampel_n_sigma=3),
-                detectorMetaData=dict(subtype="ct-highwatermark-detector")
-            )
+            hyperparams=dict(upper_weak_multiplier=1.05, upper_strong_multiplier=1.10, 
+                hampel_window_size=10, hampel_n_sigma=3, strategy="highwatermark")
+        )
         detector_class = build_detector("constant-detector", detector_config)
         data = read_csv("./tests/data/latency_elb.csv", header=0, usecols=[1], squeeze=True)
-        detector_class.train(data, "LATENCY_AVG")
+        detector_class.train(data, "LATENCY")
         print("detector model data after training:\n", related.to_json(detector_class))
-        assert isclose(detector_class.config.params.thresholds.upper_strong_threshold, 0.612911, rel_tol=0.0001)
-        assert isclose(detector_class.config.params.thresholds.upper_weak_threshold, 0.585052, rel_tol=0.0001)
-
-    
-
-   
+        assert isclose(detector_class.config.params.thresholds.strong_upper_threshold, 0.612911, rel_tol=0.0001)
+        assert isclose(detector_class.config.params.thresholds.weak_upper_threshold, 0.585052, rel_tol=0.0001)
